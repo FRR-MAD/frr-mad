ec5df6ad101878b1b1229b0b1bcea181880f78f1
Roman Cvijanovic <roman.cvijanovic@datapark.ch>
2025-04-14 12:12:57 +0200

    feat: socket testing

--- backend/internal/comms/socket/processing.go
+++ backend/internal/comms/socket/processing.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/ba2025-ysmprc/frr-tui/backend/internal/aggregator"
 	frrProto "github.com/ba2025-ysmprc/frr-tui/backend/pkg"
 )
 
+// func processCommand(message *frrProto.Message) *frrProto.Response {
 func processCommand(message *frrProto.Message) *frrProto.Response {
 	var response frrProto.Response
 
 	switch message.Command {
+	case "ospf":
+		response.Status = "success"
+		response.Message = "Returning magical ospf data"
+		return aggregator.OSPFNeighborDummyData()
+		// return &response
 	case "exit":
 		response.Status = "success"
 		response.Message = "Shutting system down"
