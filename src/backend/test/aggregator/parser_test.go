474123c3877bfdb79df6ec48c0c37e1ef8b73459
Yannick Städeli <yannick.staedeli@ost.ch>
2025-04-18 16:53:23 +0200

    test: write tests for parser

--- src/backend/test/aggregator/parser_test.go
+++ src/backend/test/aggregator/parser_test.go
@@ -1,6 +1,7 @@
 package aggregator_test
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -16,6 +17,8 @@ func TestParseStaticFRRConfig(t *testing.T) {
 		t.Fatalf("ParseStaticFRRConfig failed: %v", err)
 	}
 
+	fmt.Printf("ParseStaticFRRConfig: %v\n", config)
+
 	// Validate the parsed Metadata
 	if config.Hostname != "r101" {
 		t.Errorf("Expected Hostname to be 'r101', got '%s'", config.Hostname)
@@ -27,172 +30,271 @@ func TestParseStaticFRRConfig(t *testing.T) {
 		t.Error("Expected ServiceAdvancedVty to be true")
 	}
 
-	//if len(config.Interfaces) != 3 {
-	//	t.Errorf("Expected 3 interfaces, got %d", len(config.Interfaces))
-	//}
-	//
-	//// Check eth0 interface
-	//var eth0Found bool
-	//for _, iface := range config.Interfaces {
-	//	if iface.Name == "eth0" {
-	//		eth0Found = true
-	//		if iface.Area != "0.0.0.0" {
-	//			t.Errorf("Expected eth0 area to be '0.0.0.0', got '%s'", iface.Area)
-	//		}
-	//		if iface.Cost != 10 {
-	//			t.Errorf("Expected eth0 cost to be 10, got %d", iface.Cost)
-	//		}
-	//		if iface.Passive {
-	//			t.Error("Expected eth0 to not be passive")
-	//		}
-	//	}
-	//}
-	//if !eth0Found {
-	//	t.Error("Interface eth0 not found in parsed config")
-	//}
-	//
-	//// Check eth1 interface
-	//var eth1Found bool
-	//for _, iface := range config.Interfaces {
-	//	if iface.Name == "eth1" {
-	//		eth1Found = true
-	//		if iface.Area != "0.0.0.1" {
-	//			t.Errorf("Expected eth1 area to be '0.0.0.1', got '%s'", iface.Area)
-	//		}
-	//		if !iface.Passive {
-	//			t.Error("Expected eth1 to be passive")
-	//		}
-	//	}
-	//}
-	//if !eth1Found {
-	//	t.Error("Interface eth1 not found in parsed config")
-	//}
-	//
-	//// Check areas
-	//if len(config.Areas) != 2 {
-	//	t.Errorf("Expected 2 areas, got %d", len(config.Areas))
-	//}
-	//
-	//// Check area 0.0.0.0
-	//var area0Found bool
-	//for _, area := range config.Areas {
-	//	if area.ID == "0.0.0.0" {
-	//		area0Found = true
-	//		if len(area.Networks) != 1 {
-	//			t.Errorf("Expected 1 network in area 0.0.0.0, got %d", len(area.Networks))
-	//		}
-	//		if len(area.Networks) > 0 && area.Networks[0] != "192.168.1.0/24" {
-	//			t.Errorf("Expected network '192.168.1.0/24', got '%s'", area.Networks[0])
-	//		}
-	//	}
-	//}
-	//if !area0Found {
-	//	t.Error("Area 0.0.0.0 not found in parsed config")
-	//}
+	if len(config.Interfaces) != 12 {
+		t.Errorf("Expected 13 interfaces, got %d", len(config.Interfaces))
+	}
+
+	eth1 := config.Interfaces[0]
+	if eth1.Name != "eth1" {
+		t.Errorf("Expected first interface name to be 'eth1', got '%s'", eth1.Name)
+	}
+	if len(eth1.IpAddress) == 0 {
+		t.Error("Expected eth1 to have at least one IP address")
+	} else {
+		if eth1.IpAddress[0].IpAddress != "172.22.1.1" {
+			t.Errorf("Expected eth1 IP to be 172.22.1.1, got '%s'", eth1.IpAddress[0].IpAddress)
+		}
+		if eth1.IpAddress[0].PrefixLength != 24 {
+			t.Errorf("Expected eth1 prefix length to be 24, got %d", eth1.IpAddress[0].PrefixLength)
+		}
+	}
+
+	eth2 := config.Interfaces[1]
+	if eth2.Name != "eth2" {
+		t.Errorf("Expected second interface name to be 'eth2', got '%s'", eth2.Name)
+	}
+	if len(eth2.IpAddress) == 0 {
+		t.Error("Expected eth2 to have at least one IP address")
+	} else {
+		if eth2.IpAddress[0].IpAddress != "10.0.12.1" {
+			t.Errorf("Expected eth2 IP to be 172.22.1.1, got '%s'", eth2.IpAddress[0].IpAddress)
+		}
+		if eth2.IpAddress[0].PrefixLength != 24 {
+			t.Errorf("Expected eth2 prefix length to be 24, got %d", eth2.IpAddress[0].PrefixLength)
+		}
+	}
+	if eth2.Area != "0.0.0.0" {
+		t.Errorf("Expected eth2 to be in Area '0.0.0.0', got '%s'", eth2.Area)
+	}
+	if eth2.Passive {
+		t.Errorf("Expected eth2 to be in Passive 'false', got '%v'", eth2.Passive)
+	}
+
+	// eth3
+	eth3 := config.Interfaces[2]
+	if eth3.Name != "eth3" {
+		t.Errorf("Expected eth3 name to be 'eth3', got '%s'", eth3.Name)
+	}
+	if len(eth3.IpAddress) == 0 {
+		t.Error("Expected eth3 to have at least one IP address")
+	} else {
+		if eth3.IpAddress[0].IpAddress != "10.0.13.1" {
+			t.Errorf("Expected eth3 IP to be 10.0.13.1, got '%s'", eth3.IpAddress[0].IpAddress)
+		}
+		if eth3.IpAddress[0].PrefixLength != 24 {
+			t.Errorf("Expected eth3 prefix length to be 24, got %d", eth3.IpAddress[0].PrefixLength)
+		}
+	}
+	if eth3.Area != "0.0.0.0" {
+		t.Errorf("Expected eth3 to be in Area '0.0.0.0', got '%s'", eth3.Area)
+	}
+	if eth3.Passive {
+		t.Error("Expected eth3 to be non-passive")
+	}
+
+	// eth4
+	eth4 := config.Interfaces[3]
+	if eth4.Name != "eth4" {
+		t.Errorf("Expected eth4 name to be 'eth4', got '%s'", eth4.Name)
+	}
+	if len(eth4.IpAddress) == 0 {
+		t.Error("Expected eth4 to have at least one IP address")
+	} else {
+		if eth4.IpAddress[0].IpAddress != "10.0.0.1" {
+			t.Errorf("Expected eth4 IP to be 10.0.0.1, got '%s'", eth4.IpAddress[0].IpAddress)
+		}
+		if eth4.IpAddress[0].PrefixLength != 23 {
+			t.Errorf("Expected eth4 prefix length to be 23, got %d", eth4.IpAddress[0].PrefixLength)
+		}
+	}
+	if eth4.Area != "0.0.0.0" {
+		t.Errorf("Expected eth4 to be in Area '0.0.0.0', got '%s'", eth4.Area)
+	}
+	if !eth4.Passive {
+		t.Error("Expected eth4 to be passive")
+	}
+
+	// eth11
+	eth11 := config.Interfaces[10]
+	if eth11.Name != "eth11" {
+		t.Errorf("Expected eth11 name to be 'eth11', got '%s'", eth11.Name)
+	}
+	if len(eth11.IpAddress) == 0 {
+		t.Error("Expected eth11 to have at least one IP address")
+	} else {
+		if eth11.IpAddress[0].IpAddress != "10.0.19.1" {
+			t.Errorf("Expected eth11 IP to be 10.0.19.1, got '%s'", eth11.IpAddress[0].IpAddress)
+		}
+		if eth11.IpAddress[0].PrefixLength != 24 {
+			t.Errorf("Expected eth11 prefix length to be 24, got %d", eth11.IpAddress[0].PrefixLength)
+		}
+	}
+	if eth11.Area != "0.0.0.0" {
+		t.Errorf("Expected eth11 to be in Area '0.0.0.0', got '%s'", eth11.Area)
+	}
+	if eth11.Passive {
+		t.Error("Expected eth11 to be non-passive")
+	}
+
+	// lo
+	lo := config.Interfaces[11]
+	if lo.Name != "lo" {
+		t.Errorf("Expected lo interface name to be 'lo', got '%s'", lo.Name)
+	}
+	if len(lo.IpAddress) == 0 {
+		t.Error("Expected lo to have at least one IP address")
+	} else {
+		if lo.IpAddress[0].IpAddress != "65.0.1.1" {
+			t.Errorf("Expected lo IP to be 65.0.1.1, got '%s'", lo.IpAddress[0].IpAddress)
+		}
+		if lo.IpAddress[0].PrefixLength != 32 {
+			t.Errorf("Expected lo prefix length to be 32, got %d", lo.IpAddress[0].PrefixLength)
+		}
+	}
+	if !lo.Passive {
+		t.Error("Expected lo to be passive")
+	}
+
+	staticRoutes := config.StaticRoutes
+	if len(staticRoutes) != 1 {
+		t.Errorf("Expected 1 static route, got %d", len(staticRoutes))
+	}
+	if staticRoutes[0].IpPrefix.IpAddress != "192.168.1.0" {
+		t.Errorf("Expected static route one to have 192.168.1.0, got '%s'", staticRoutes[0].IpPrefix)
+	}
+	if staticRoutes[0].IpPrefix.PrefixLength != 24 {
+		t.Errorf("Expected static route Prefix one to be 24, got %d", staticRoutes[0].IpPrefix.PrefixLength)
+	}
+	if staticRoutes[0].NextHop != "192.168.100.91" {
+		t.Errorf("Expected Next Hop to be 192.168.100.91, got '%s'", staticRoutes[0].NextHop)
+	}
+
+	ospfConfig := config.OspfConfig
+	if ospfConfig.RouterId != "65.0.1.1" {
+		t.Errorf("Expected ospf router id 65.0.1.1, got '%s'", ospfConfig.RouterId)
+	}
+	if ospfConfig.Redistribution[0].Type != "static" {
+		t.Errorf("Expected ospf redistribution type static, got '%s'", ospfConfig.Redistribution[0].Type)
+	}
+	if ospfConfig.Redistribution[0].Metric != "1" {
+		t.Errorf("Expected Metric to be '1', got '%s'", ospfConfig.Redistribution[0].Metric)
+	}
+	if ospfConfig.Redistribution[0].RouteMap != "lanroutes" {
+		t.Errorf("Expected RouteMap to be 'lanroutes', got '%s'", ospfConfig.Redistribution[0].RouteMap)
+	}
+	if ospfConfig.Redistribution[1].Type != "bgp" {
+		t.Errorf("Expected Redistribution type bgp, got '%s'", ospfConfig.Redistribution[1].Type)
+	}
+	if ospfConfig.Redistribution[1].Metric != "1" {
+		t.Errorf("Expected Metric to be '1', got '%s'", ospfConfig.Redistribution[1].Metric)
+	}
+	if ospfConfig.Redistribution[1].RouteMap != "" {
+		t.Errorf("Expected RouteMap to be empty, got '%s'", ospfConfig.Redistribution[1].RouteMap)
+	}
 }
 
-func TestParseConfig(t *testing.T) {
-	// Test valid config
-	tempDir := t.TempDir()
-	configPath := filepath.Join(tempDir, "ospf.conf")
-
-	configData := `! OSPF Configuration
-interface eth0
- ip ospf area 0.0.0.0
- ip ospf cost 10
-
-interface eth1
- ip ospf area 0.0.0.1
- ip ospf passive
-
-router ospf
- ospf router-id 192.168.1.1
- network 192.168.1.0/24 area 0.0.0.0
- network 192.168.2.0/24 area 0.0.0.1
-exit
-
-interface eth2
- ip ospf area 0.0.0.0
-`
-
-	err := os.WriteFile(configPath, []byte(configData), 0644)
-	if err != nil {
-		t.Fatalf("Failed to write test config file: %v", err)
-	}
-
-	config, err := aggregator.ParseStaticFRRConfig(configPath)
-	if err != nil {
-		t.Fatalf("ParseStaticFRRConfig failed: %v", err)
-	}
-
-	// Validate the parsed config
-	if config.RouterID != "192.168.1.1" {
-		t.Errorf("Expected RouterID to be '192.168.1.1', got '%s'", config.RouterID)
-	}
-
-	if len(config.Interfaces) != 3 {
-		t.Errorf("Expected 3 interfaces, got %d", len(config.Interfaces))
-	}
-
-	// Check eth0 interface
-	var eth0Found bool
-	for _, iface := range config.Interfaces {
-		if iface.Name == "eth0" {
-			eth0Found = true
-			if iface.Area != "0.0.0.0" {
-				t.Errorf("Expected eth0 area to be '0.0.0.0', got '%s'", iface.Area)
-			}
-			if iface.Cost != 10 {
-				t.Errorf("Expected eth0 cost to be 10, got %d", iface.Cost)
-			}
-			if iface.Passive {
-				t.Error("Expected eth0 to not be passive")
-			}
-		}
-	}
-	if !eth0Found {
-		t.Error("Interface eth0 not found in parsed config")
-	}
-
-	// Check eth1 interface
-	var eth1Found bool
-	for _, iface := range config.Interfaces {
-		if iface.Name == "eth1" {
-			eth1Found = true
-			if iface.Area != "0.0.0.1" {
-				t.Errorf("Expected eth1 area to be '0.0.0.1', got '%s'", iface.Area)
-			}
-			if !iface.Passive {
-				t.Error("Expected eth1 to be passive")
-			}
-		}
-	}
-	if !eth1Found {
-		t.Error("Interface eth1 not found in parsed config")
-	}
-
-	// Check areas
-	if len(config.Areas) != 2 {
-		t.Errorf("Expected 2 areas, got %d", len(config.Areas))
-	}
-
-	// Check area 0.0.0.0
-	var area0Found bool
-	for _, area := range config.Areas {
-		if area.ID == "0.0.0.0" {
-			area0Found = true
-			if len(area.Networks) != 1 {
-				t.Errorf("Expected 1 network in area 0.0.0.0, got %d", len(area.Networks))
-			}
-			if len(area.Networks) > 0 && area.Networks[0] != "192.168.1.0/24" {
-				t.Errorf("Expected network '192.168.1.0/24', got '%s'", area.Networks[0])
-			}
-		}
-	}
-	if !area0Found {
-		t.Error("Area 0.0.0.0 not found in parsed config")
-	}
-}
+//func TestParseConfig(t *testing.T) {
+//	// Test valid config
+//	tempDir := t.TempDir()
+//	configPath := filepath.Join(tempDir, "ospf.conf")
+//
+//	configData := `! OSPF Configuration
+//interface eth0
+// ip ospf area 0.0.0.0
+// ip ospf cost 10
+//
+//interface eth1
+// ip ospf area 0.0.0.1
+// ip ospf passive
+//
+//router ospf
+// ospf router-id 192.168.1.1
+// network 192.168.1.0/24 area 0.0.0.0
+// network 192.168.2.0/24 area 0.0.0.1
+//exit
+//
+//interface eth2
+// ip ospf area 0.0.0.0
+//`
+//
+//	err := os.WriteFile(configPath, []byte(configData), 0644)
+//	if err != nil {
+//		t.Fatalf("Failed to write test config file: %v", err)
+//	}
+//
+//	config, err := aggregator.ParseStaticFRRConfig(configPath)
+//	if err != nil {
+//		t.Fatalf("ParseStaticFRRConfig failed: %v", err)
+//	}
+//
+//	// Validate the parsed config
+//	if config.RouterID != "192.168.1.1" {
+//		t.Errorf("Expected RouterID to be '192.168.1.1', got '%s'", config.RouterID)
+//	}
+//
+//	if len(config.Interfaces) != 3 {
+//		t.Errorf("Expected 3 interfaces, got %d", len(config.Interfaces))
+//	}
+//
+//	// Check eth0 interface
+//	var eth0Found bool
+//	for _, iface := range config.Interfaces {
+//		if iface.Name == "eth0" {
+//			eth0Found = true
+//			if iface.Area != "0.0.0.0" {
+//				t.Errorf("Expected eth0 area to be '0.0.0.0', got '%s'", iface.Area)
+//			}
+//			if iface.Cost != 10 {
+//				t.Errorf("Expected eth0 cost to be 10, got %d", iface.Cost)
+//			}
+//			if iface.Passive {
+//				t.Error("Expected eth0 to not be passive")
+//			}
+//		}
+//	}
+//	if !eth0Found {
+//		t.Error("Interface eth0 not found in parsed config")
+//	}
+//
+//	// Check eth1 interface
+//	var eth1Found bool
+//	for _, iface := range config.Interfaces {
+//		if iface.Name == "eth1" {
+//			eth1Found = true
+//			if iface.Area != "0.0.0.1" {
+//				t.Errorf("Expected eth1 area to be '0.0.0.1', got '%s'", iface.Area)
+//			}
+//			if !iface.Passive {
+//				t.Error("Expected eth1 to be passive")
+//			}
+//		}
+//	}
+//	if !eth1Found {
+//		t.Error("Interface eth1 not found in parsed config")
+//	}
+//
+//	// Check areas
+//	if len(config.Areas) != 2 {
+//		t.Errorf("Expected 2 areas, got %d", len(config.Areas))
+//	}
+//
+//	// Check area 0.0.0.0
+//	var area0Found bool
+//	for _, area := range config.Areas {
+//		if area.ID == "0.0.0.0" {
+//			area0Found = true
+//			if len(area.Networks) != 1 {
+//				t.Errorf("Expected 1 network in area 0.0.0.0, got %d", len(area.Networks))
+//			}
+//			if len(area.Networks) > 0 && area.Networks[0] != "192.168.1.0/24" {
+//				t.Errorf("Expected network '192.168.1.0/24', got '%s'", area.Networks[0])
+//			}
+//		}
+//	}
+//	if !area0Found {
+//		t.Error("Area 0.0.0.0 not found in parsed config")
+//	}
+//}
 
 func TestParseConfigErrors(t *testing.T) {
 	// Test nonexistent file
